859c29e1d4e255d0b7fe0f0a7d62ac7f89d2745d
Juan Carlos Nuñez Chavarria <134462352+JuankChavarria@users.noreply.github.com>
2025-06-20 11:30:37 -0600

    Agregar archivos de proyecto.

---
+++ Progra-VI-Tienda/Progra-VI-Tienda/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Progra_VI_Tienda.Models;
+using System.Diagnostics;
+
+namespace Progra_VI_Tienda.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Progra-VI-Tienda/Progra-VI-Tienda/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Progra_VI_Tienda.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
